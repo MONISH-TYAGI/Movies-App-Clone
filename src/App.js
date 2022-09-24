2d0ca435b701824c8f7499743ddb29fbe6cc64bf
MONISH_TYAGI <monishtyagi5770@gmail.com>
2022-09-25 01:10:27 +0530

    25sept22

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import Banner from './Components/Banner';
+import Movies from './Components/Movies';
+import Favourite from './Components/Favourite';
+
+import {BrowserRouter as Router,Routes,Route,BrowserRouter} from 'react-router-dom';
+function App() {
+  return (
+    <Router>
+   <Navbar/>
+   {/* <Banner/>
+   <Movies/> */}
+   <Routes>
+   <Route path='/' element={[<Banner/>,<Movies/>]}/>
+    {/* <Route  exact path='/' render={(props)=>(
+      <>
+      <Banner {...props}/>
+      <Movies {...props}/>
+      </>
+    )}/> */}
+    <Route path='/favourites' element={<Favourite/>}/>
+   </Routes>
+   {/* <Favourite/> */}
+   </Router>
+  );
+}
+
+export default App;
